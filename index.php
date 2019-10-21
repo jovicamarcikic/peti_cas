219cf59fc07056c1524dafac206c857ccb9a08c7
jovicamarcikic <jovicamarcikic@gmail.com>
2019-10-21 18:24:32 +0200

    Dodatak

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
 $konekcija = mysqli_connect('localhost', 'root', "", "kuca");
+$korisnici = mysqli_query($konekcija, "SELECT * FROM korisnici");
+
+echo $korisnici->num_rows;
+echo "<br>";
+
+if ($korisnici->num_rows > 0)
+{
+    echo "Pronadeni podaci";
+    while ($redubazi = $korisnici->fetch_assoc())
+    {
+        echo $redubazi["Ime"];
+    }
+}
